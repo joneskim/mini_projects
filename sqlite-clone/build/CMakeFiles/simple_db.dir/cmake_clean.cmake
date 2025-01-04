f2ebb0657bbd316322cd7fd3e6c5437adc35f9a1
joneskim <joneskimaminiel@gmail.com>
2025-01-04 11:43:42 -0600

    sqlite clone following tutorial. I am done implementing the REPL

---
+++ sqlite-clone/build/CMakeFiles/simple_db.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_db.dir/main.c.o"
+  "CMakeFiles/simple_db.dir/main.c.o.d"
+  "simple_db"
+  "simple_db.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple_db.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
