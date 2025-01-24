d7c413e6850756892f5cd625cc698a5b8981eddd
joneskim <joneskimaminiel@gmail.com>
2025-01-23 21:37:20 -0600

    basic logger to a text file working

--- Logger/logger.cpp
+++ Logger/logger.cpp
@@ -2,23 +2,47 @@
 #include <fstream>
 #include <string>
 
-enum class LogLevel {
+enum class logLevel {
     INFO,
     WARNING,
-    ERROR
+    ERROR,
 };
 
 
 class Logger {
     public:
-        void log(logLevel level, const std::string& message){
-            
+        static Logger& getInstance(){
+            static Logger instance("log.txt");
+            return instance;
         }
+        void log(logLevel lvl, const std::string& message){
+            std::string levelStr;
+            switch (lvl){
+                case logLevel::INFO:
+                    levelStr = "INFO";
+                    break;
+                case logLevel::WARNING:
+                    levelStr = "WARNING";
+                    break;
+                case logLevel::ERROR:
+                    levelStr = "ERROR";
+                    break;
+                default:
+                    levelStr = "UNKNOWN";
+                    break;
+            }   
+
+            file << levelStr << ": " << message << '\n';
+        }
+
+        // we can delete the copy constructor and assignment operator
+        Logger(const Logger&) = delete;
+        Logger& operator=(const Logger&) = delete;
 
     private:
 
         // the constructor opens the file
-        Logger(const std::string& logFile = "log.txt") : logFile(logFile) {
+        Logger(const std::string& logFile) {
             file.open(logFile);
             if (!file.is_open()) {
                 std::cerr << "Error opening log file" << std::endl;
@@ -35,4 +59,13 @@ class Logger {
 
         std::string logFile;
         std::ofstream file;
+};
+
+int main() {
+    Logger& logger = Logger::getInstance();
+    logger.log(logLevel::INFO, "This is an informational message");
+    logger.log(logLevel::WARNING, "This is a warning message");
+    logger.log(logLevel::ERROR, "This is an error message");
+
+    return 0;
 }
